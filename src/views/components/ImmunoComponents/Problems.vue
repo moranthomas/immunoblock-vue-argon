6b33255146eb189c5485c9148a5271a330d4b370
moranthomas <thomas@optimizme.com>
2020-01-05 15:50:39 +0000

    finish problems section

--- src/views/components/ImmunoComponents/Problems.vue
+++ src/views/components/ImmunoComponents/Problems.vue
@@ -13,10 +13,36 @@
             <li>50 million Americans suffer from an autoimmune related disease (according to AARDA*).</li>
             <li>Autoimmunity is now one of the most common disease categories, ahead of cancer and heart disease.</li>
             <li>It is now one of the top 10 leading causes of death in female children and women.</li>
-            <li>Unlike cancer, there’s no central database to help researchers understand how many people are affected, where cases are occurring and how incidences are rising.</li>
+            <li>Unlike cancer, there’s no central database to help researchers understand changes in incidence and prevalence.</li>
           </ul>
 
             <problem-images></problem-images>
+
+            </br>
+            </br>
+
+          <h4 class="h5 flex-column flex-md-row  mb-4">Protecting and Sharing Patient Data</h4>
+
+          <ul>
+            <li>Maintaining the Security and integrity of patient data is a serious problem when sharing health records.</li>
+            <li>Healthcare fraud and attacks cost a lot of money - The WannaCry ransomware attack in 2017 cost the NHS £92 million.</li>
+            <li>HIPPA fines cost the top 10 companies a total of $28.7 million in 2018.</li>
+            <li>Estimates indicate that as many as 150 to 400 individuals may access a person’s Electronic Health Record.</li>
+            <li>In the UK only 16% of patients know how their medical data is used by commercial organizations and academic researchers.</li>
+          </ul>
+
+            </br>
+            </br>
+
+
+          <h4 class="h5 flex-column flex-md-row  mb-4">Finding The Right Match</h4>
+          <ul>
+            <li>Pharma and healthcare facilities are always on the lookout for new patients for their research activities..</li>
+            <li>They spend a huge amount of money and time finding, qualifying, and onboarding the right candidates for their clinical trials.</li>
+            <li>Only 10 % of new drugs make it to the market, so it’s critical to identify and recruit the perfect candidates.</li>
+            <li>To get access, research companies usually go through intermediaries.</li>
+          </ul>
+
             </br>
             </br>
 
